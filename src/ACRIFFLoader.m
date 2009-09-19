bf4dcb3350f492334a6b9dde30c0a8428c5ddc7d
The Stig <the.git.dev@gmail.com>
2009-09-19 11:46:45 -0400

    Reading RIFF documents works properly

--- src/ACRIFFLoader.m
+++ src/ACRIFFLoader.m
@@ -162,7 +162,6 @@
 		while (i >= 0)
 		{
 			NSXMLElement* layer = [layers objectAtIndex: i];
-			i--;
 			
 			BOOL visible = [[[[layer nodesForXPath:@"@offset-x" error: nil] objectAtIndex: 0] objectValue] boolValue];
 			int offsetx = [[[[layer nodesForXPath:@"@offset-x" error: nil] objectAtIndex: 0] objectValue] intValue];
@@ -180,15 +179,21 @@
 				id <ACGroupLayer> baseGroup = [document baseGroup];
 				id <ACBitmapLayer> layer    = [baseGroup insertCGImage: imageRef atIndex:0 withName: name];
 				
-				if (i > 0)
+				// Base layer should ignore these values
+				if (i == 0)
 				{
-					[layer setVisible: visible];
-					[layer setOpacity: opacity];
-					NSPoint drawDelta = NSMakePoint(offsetx, docHeight - offsety);
-					[layer setDrawDelta: drawDelta];
+					opacity = 1.0f;
+					mode = kCGBlendModeNormal;
+					offsetx = 0;
+					offsety = 0;
 				}
+				[layer setVisible: visible];
+				[layer setOpacity: opacity];
+				NSPoint drawDelta = NSMakePoint(offsetx, docHeight - (offsety + CGImageGetHeight(imageRef)));
+				[layer setDrawDelta: drawDelta];
 				
 				CGImageRelease(imageRef);
+				i--;
 
 				result = YES;
 			}
@@ -198,101 +203,6 @@
 	[xml release];
 
 	return result;
-#if 0
-	NSSize canvasSize = NSMakeSize(pdfContext->width, pdfContext->height);
-	[document setCanvasSize:canvasSize];
-	[document setDpi:NSMakeSize(pdfContext->resolution_info.hres, pdfContext->resolution_info.hres)];
-	
-	int i = pdfContext->layer_count;
-	while (i > 0) {
-		psd_layer_record psdLayer = pdfContext->layer_records[i - 1];
-		i--;
-		
-		if ((psdLayer.width == 0) || (!psdLayer.image_data) || (!*psdLayer.image_data)) {
-			debug(@"no dater for layer %d", i);
-			continue;
-		}
-		
-		
-		CGContextRef context;
-		size_t destBytesPerRow = psdLayer.width * 4;
-		//destBytesPerRow = COMPUTE_BEST_BYTES_PER_ROW(destBytesPerRow);
-		psd_argb_color *outDest = calloc(1, destBytesPerRow * psdLayer.height);
-		
-		//#define PSD_VIMAGE
-		
-#ifdef PSD_VIMAGE
-		
-		vImage_Buffer src  = { psdLayer.image_data, psdLayer.width, psdLayer.height, destBytesPerRow };
-		vImage_Buffer dest = { outDest, psdLayer.width, psdLayer.height, psdLayer.width * 4 };
-		vImagePremultiplyData_ARGB8888(&src, &dest, kvImageNoFlags);
-		
-#else
-		psd_argb_color * dst_color = outDest;
-		psd_argb_color * src_color = psdLayer.image_data;
-		int j = psdLayer.width * psdLayer.height;
-		while (j > 0) {
-			j--;
-			psd_argb_color temp = *src_color;
-			
-			//debug(@"temp: %x", temp);
-			//debug(@"j: %d", j);
-			
-			psd_uchar sA = ACPSDGetAlpha(temp);
-			psd_uchar sR = ACPSDGetRed(temp);
-			psd_uchar sG = ACPSDGetGreen(temp);
-			psd_uchar sB = ACPSDGetBlue(temp);
-			
-			psd_uchar dR = (sR * sA + 127) / 255;
-			psd_uchar dG = (sG * sA + 127) / 255;
-			psd_uchar dB = (sB * sA + 127) / 255;
-			psd_uchar dA = sA;
-			
-			*dst_color = CFSwapInt32BigToHost(ACPSDARGBToColor(dA, dR, dG, dB));
-			
-			src_color++;
-			dst_color++;
-		}
-#endif
-		
-		context = CGBitmapContextCreate(outDest, psdLayer.width, psdLayer.height, 8, destBytesPerRow, 
-																		CGColorSpaceCreateWithName(kCGColorSpaceGenericRGB),
-																		kCGImageAlphaPremultipliedFirst);
-		
-		if (context) {
-			CGImageRef imageRef = CGBitmapContextCreateImage(context);
-			
-			NSString *name = [NSString stringWithUTF8String:(const char *)psdLayer.layer_name];
-			
-			id <ACGroupLayer> baseGroup = [document baseGroup];
-			id <ACBitmapLayer> layer    = [baseGroup insertCGImage:imageRef atIndex:0 withName:name];
-			
-			[layer setVisible:psdLayer.visible];
-			
-			[layer setOpacity:psdLayer.opacity / 255.0f];
-			
-			NSPoint drawDelta = NSMakePoint(psdLayer.left, canvasSize.height - psdLayer.bottom);
-			
-			[layer setDrawDelta:drawDelta];
-			
-			CGImageRelease(imageRef);
-		}
-		
-		CGContextRelease(context);
-		free(outDest);
-		
-		
-	}
-	
-	// free if it's done
-	psd_image_free(pdfContext);
-	
-	if ([[[document baseGroup] layers] count] == 0) {
-		return [self loadCompoisteForDocument:document fromURL:absoluteURL];
-	}
-	
-	return YES;
-#endif
 }
 
 - (NSNumber*) worksOnShapeLayers: (id) userObject
